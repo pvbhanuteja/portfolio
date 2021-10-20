13dbe86b56bbbf91b96ab5440c921ebd36ae9496
Bhanu <pvbhanuteja@gmail.com>
2021-10-19 23:55:01 -0500

    added 2 resume pages

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -29,9 +29,15 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
+        <Row className="resume mx-auto">
           <Document file={resumeLink}>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
+
+        <Row className="resume">
+          <Document file={resumeLink}>
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
